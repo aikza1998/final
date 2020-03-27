585cb73a3affe536e63a4397813b1d5d1bb2466a
aikza1998 <60310075@go.buu.ac.th>
2020-03-27 15:32:20 +0700

    q
    q

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     </head>
   <body>
     <%- include layouts %>
-    <h1><%= title %></h1>
+    <h1><%= titleww %></h1>
     <p>Welcome t <%= title %></p>
 
     <b>List work</b> <br>
-    Form by 60310075  => <a href="https://simplenodejsexpress.herokuapp.com/form">FORM</a>
+    Form by 60310075  => <a href="https://manussawee.herokuapp.com">FORM</a>
     <br>
-    Form validation by 60310075 => <a href="https://simplenodejsexpress.herokuapp.com/validation">Validation</a> 
+    Form validation by 60310075 => <a href="test.ejs">Validation</a> 
   </div>
   </body>
 </html>
