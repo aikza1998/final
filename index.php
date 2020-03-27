8034c31188288088860bf6c4f1ca8d04335187ea
aikza1998 <60310075@go.buu.ac.th>
2020-03-27 16:35:48 +0700

    edit_by_manussawee
    60310075

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
     <meta charset="utf-8" />
     </head>
   <body>
+    <center>
  
     <h1> FINAL </h1>
     <p>Welcome to final </p>
 
-    <b>List work</b> <br>
-    test_final by 60310075  => <a href="final.html">test</a>
+    <b>List work</b> <br><font color="red">
+    final by 60310075 มนัสวี นพฤทธิ์ ไอที ปี 3 => <a href="final.html">final</a> 
     <br>
-    final by 60310075 => <a href="final.html">final</a> 
+    test_final by 60310075 Manussawee Nopparit IT_3  => <a href="final.html">test</a></font>
+  </center>
   </div>
   </body>
 </html>
