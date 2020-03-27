5803602541c979166f096d86932a0e1e620e0ed1
aikza1998 <60310075@go.buu.ac.th>
2020-03-27 16:18:55 +0700

    60310075
    60310075manussawee

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title><%= title %></title>
+    <title> title </title>
     </head>
   <body>
     <%- include layouts %>
-    <h1><%= titleww %></h1>
-    <p>Welcome t <%= title %></p>
+    <h1> FINAL </h1>
+    <p>Welcome to final </p>
 
     <b>List work</b> <br>
-    Form by 60310075  => <a href="https://manussawee.herokuapp.com">FORM</a>
+    Form by 60310075  => <a href="https://manussawee.herokuapp.com">test</a>
     <br>
-    Form validation by 60310075 => <a href="test.ejs">Validation</a> 
+    final by 60310075 => <a href="final.html">final</a> 
   </div>
   </body>
 </html>
